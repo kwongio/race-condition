d6e9e5f08f57486d37566d0851f4459a99b85fac
kwongio <rldh11111@naver.com>
2024-02-27 12:54:29 +0900

    [rename] 클래스 이름 수정

--- src/test/java/com/example/stock/service/LettuceLockStockFacadeTest.java
+++ src/test/java/com/example/stock/service/LettuceLockStockFacadeTest.java
@@ -15,7 +15,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @SpringBootTest
-class LettuceLockStockServiceTest {
+class LettuceLockStockFacadeTest {
 
 
     @Autowired
