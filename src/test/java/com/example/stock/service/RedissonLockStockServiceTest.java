ebb909d242c3ba399b8b9b09b1f3e8eba4280b18
kwongio <rldh11111@naver.com>
2024-02-27 12:53:48 +0900

    [feat] redisson Lock을 사용해 동시성 문제 해결

---
+++ src/test/java/com/example/stock/service/RedissonLockStockServiceTest.java
@@ -0,0 +1,65 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.facade.LettuceLockStockFacade;
+import com.example.stock.facade.RedissonLockStockFacade;
+import com.example.stock.repository.StockRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+class RedissonLockStockServiceTest {
+
+
+    @Autowired
+    private RedissonLockStockFacade redissonLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+
+    @BeforeEach
+    public void before() {
+        stockRepository.save(Stock.builder()
+                .productId(1L)
+                .quantity(100L)
+                .build());
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    redissonLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        Assertions.assertThat(stock.getQuantity()).isEqualTo(0L);
+        System.out.println(stock.getQuantity());
+    }
+
+
+}
